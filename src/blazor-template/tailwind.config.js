be2dad6a7903a9c46daa2a11aeebe1f06d2eee6f
Christopher Pike <chris@cpike.ca>
2025-08-16 09:09:39 -0400

    Switching layout components to tailwind

---
+++ src/blazor-template/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Components/**/*.{razor,html,cshtml}',
+    './Pages/**/*.{razor,html,cshtml}',
+    './Views/**/*.{razor,html,cshtml}',
+    './**/*.razor',
+    '!./node_modules/**'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#593c63',
+        secondary: '#ba4c44',
+        info: '#d29238',
+        success: '#1ea369',
+        warning: '#dccd1e',
+        danger: '#e8320e',
+        dark: '#0c0100',
+        light: '#e3e5eb'
+      }
+    }
+  },
+  plugins: []
+}
